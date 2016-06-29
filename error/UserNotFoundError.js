e7d04e02e9bc733ff9d7d2e349cdb476e6b4ba0e
Mark Ross <mross@inviqa.com>
2016-06-29 17:46:24 +0100

    Remove commented code

--- error/UserNotFoundError.js
+++ error/UserNotFoundError.js
@@ -1,15 +1,3 @@
-// function UserNotFoundError(message) {
-//     this.message = message;
-//     this.name = "MyCustomError";
-//     Error.captureStackTrace(this, UserNotFoundError);
-// }
-// UserNotFoundError.prototype = Object.create(Error.prototype);
-// UserNotFoundError.prototype.constructor = UserNotFoundError;
-//
-// module.exports = UserNotFoundError;
-
-
-
 class UserNotFoundError {
     constructor(message) {
         this.name = 'UserNotFoundError';
